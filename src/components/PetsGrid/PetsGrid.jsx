ebb4dc1ed0b2304c555e148013a32627e64f5c2a
KosmoPirat <kosmopirat@gmail.com>
2020-01-28 15:46:36 +0300

    Рефакторинг PetsGrid и PetsLayout

--- src/components/PetsGrid/PetsGrid.jsx
+++ src/components/PetsGrid/PetsGrid.jsx
@@ -1,36 +1,18 @@
 import { h } from 'preact';
-import { useState, useEffect } from 'preact/hooks';
-import contentfulClient from '../../helpers/contentful/contentfulClient';
 import GridCard from '../GridCard/GridCard';
 
 import style from './PetsGrid.css';
 
-const PetsGrid = () => {
-    const [petItems, changePetItems] = useState([]);
-    const [arePetsLoaded, changeArePetsLoaded] = useState(false);
-
-    useEffect(() => {
-        contentfulClient.getAllPets().then(pets => {
-            changePetItems(pets.items);
-            changeArePetsLoaded(true);
-        });
-    }, []);
-
-    if (!arePetsLoaded) {
-        return null;
+const PetsGrid = pets => {
+    if (!pets.petsList.length) {
+        return <div>error loading pet data</div>;
     }
 
-    return (
-        <section className={style['pets-grid']}>
-            {petItems.map(pet => (
-                <GridCard
-                    slug={pet.fields.slug}
-                    name={pet.fields.name}
-                    mainPhoto={pet.fields.mainPhoto}
-                />
-            ))}
-        </section>
-    );
+    const listOfPets = pets.petsList.map(pet => (
+        <GridCard slug={pet.fields.slug} name={pet.fields.name} mainPhoto={pet.fields.mainPhoto} />
+    ));
+
+    return <section className={style['pets-grid']}>{listOfPets}</section>;
 };
 
 export default PetsGrid;
--- src/components/PetsLayout/PetsLayout.jsx
+++ src/components/PetsLayout/PetsLayout.jsx
@@ -1,15 +1,27 @@
 import { h } from 'preact';
 
+import { useEffect, useState } from 'preact/hooks';
 import PetsGrid from '../PetsGrid/PetsGrid';
 import PetsFilter from '../PetsFilter/PetsFilter';
 
 import style from './PetsLayout.css';
+import contentfulClient from '../../helpers/contentful/contentfulClient';
 
 const PetsLayout = () => {
+    const [petItems, changePetItems] = useState([]);
+    const [arePetsLoaded, changeArePetsLoaded] = useState(false);
+
+    useEffect(() => {
+        contentfulClient.getAllPets().then(pets => {
+            changePetItems(pets.items);
+            changeArePetsLoaded(true);
+        });
+    }, []);
+
     return (
         <div className={style['pets-layout']}>
             <PetsFilter />
-            <PetsGrid />
+            {arePetsLoaded ? <PetsGrid petsList={petItems} /> : <div>Loading...</div>}
         </div>
     );
 };
