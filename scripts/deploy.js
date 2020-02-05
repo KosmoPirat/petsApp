7b7742ea257d3f3b52df3fa71e226a2bab972bd3
Ivan Dashkevich <ivandashk.public@gmail.com>
2020-02-05 09:57:45 +0300

    Деплой (#11)

---
+++ scripts/deploy.js
@@ -0,0 +1,24 @@
+const dotenv = require('dotenv').config().parsed
+const FtpDeploy = require("ftp-deploy");
+const ftpDeploy = new FtpDeploy();
+ 
+const config = {
+    user: dotenv.FTPUser,
+    // Password optional, prompted if none given
+    password: dotenv.FTPPassword,
+    host: dotenv.FTPServer,
+    localRoot: __dirname + "/../build",
+    remoteRoot: "/public_html/remote-folder/",
+    // include: ["*", "**/*"],      // this would upload everything except dot files
+    include: ["*", "**/*"],
+    // delete ALL existing files at destination before uploading, if true
+    deleteRemote: false,
+    // Passive mode is forced (EPSV command is not sent)
+    forcePasv: true
+};
+ 
+// use with promises
+ftpDeploy
+    .deploy(config)
+    .then(res => console.log("finished:", res))
+    .catch(err => console.log(err));
