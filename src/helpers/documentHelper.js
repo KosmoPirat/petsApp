2281660e9f88339751037a9160e4824895a403e9
tau-chieftain <tau-chieftain@yandex-team.ru>
2020-06-02 17:39:42 +0300

    Рефакторинг пагинации

--- src/helpers/documentHelper.js
+++ src/helpers/documentHelper.js
@@ -2,10 +2,6 @@ class Document {
     static setTitle = text => {
         document.title = `${text} | Приют собак`;
     };
-
-    static setUrl = text => {
-        window.location.href = `${window.location.pathname}?page=${text}`;
-    };
 }
 
 export default Document;
